b5ef7f9b327654671981b035c95b284333e64625
Raaihhan <lenerjejerin@gmail.com>
2024-11-23 19:27:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,26 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Hayy Sayangku Dindakuu", "Cuma mau bilang", "Happy Anniversary", "Anjayy udah setahun aja brok ", "Betah-betah yaa jiwanya awoakwokaowk ", "Eh tapi ini serius dulu kali ya", "maaf ya kita harus LDR gini, anniv malah LDR", "inget ya sayang kita harus saling", "pinjam dulu seratus nanti ku ganti", "aku dalam segenap jiwa bener-bener sayang banget sama kamu dindaku", "Aku Sayang Banget Sama Kamu", "I Love You Ayu Dinda Nurani"],
-        typeSpeed: 70
+        strings: ["Hayy Sayangku Dindakuu", 
+        "Cuma mau bilang", 
+        "Happy Anniversary", 
+        "Anjayy udah setahun aja brok nggak kerasa ya udah sejauh ini",
+        "kata gw lah buset setahun aja yak, ga kerasa",
+        "Betah-betah yaa jiwanya awoakwokaowk ", 
+        "Eh tapi ini serius dulu kali ya", 
+        "maaf ya kita harus LDR gini, anniv malah LDR", 
+        "inget ya sayang kita harus saling.",
+        "saling percaya, saling dukung, jangan suka ngilang ya sayang, inget ya kamu ga sendirian ada aku",
+        "aku terima kasih banget kamu udah mau sabar udah mau bertahan sampe sejauh ini, aku bangga sama kamu",
+        "kamu kuat kamu keren walaupun kayak tai kadang kelauannya, cuma yaudah gapapa ya",
+        "adaa aku ya sayangku dindaku, jangan pernah ngerasa sendiri, walaupun kita jauh tapi kita menatap layar terus",
+        "soalnya kan kia gapernah keluar rumah jadi ga menatap langit",
+        "bercanda ya sayang, intinya semoga di umur hubungan kita yang setahun ini diberi kesehatan, kelancaran, langgeng, rezeki yang lancar, dan",
+        "semoga plan yang udah kita buat semua terwujud ya sayangkuu dindakuu, Aamiin....", 
+        "pinjam dulu seratus nanti ku ganti", 
+        "aku dalam segenap jiwa bener-bener sayang banget sama kamu dindaku", 
+        "Aku Sayang Banget Sama Kamu", 
+        "I Love You Ayu Dinda Nurani"],
+        typeSpeed: 80
     });
 }
