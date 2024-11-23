f2fa0af534b6b9a6f91a3f3f5677a4267e8c995c
raihan <raihanamin2212@gmail.com>
2024-11-24 06:46:57 +0700

    ok

--- main.js
+++ main.js
@@ -2,28 +2,32 @@ function clicks() {
     $('#div1').hide();
     $('#div2').show();
     $('#div2').addClass("fadeinv2");
-        
+    
+    document.getElementById("bg-music").play();
     var typed = new Typed('.katakata', {
-        strings: ["Hayy Sayangku Dindakuu", 
-        "Cuma mau bilang", 
-        "Happy Anniversary", 
-        "Anjayy udah setahun aja brok nggak kerasa ya udah sejauh ini",
-        "kata gw lah buset setahun aja yak, ga kerasa",
-        "Betah-betah yaa jiwanya awoakwokaowk ", 
-        "Eh tapi ini serius dulu kali ya", 
-        "maaf ya kita harus LDR gini, anniv malah LDR", 
-        "inget ya sayang kita harus saling.",
-        "saling percaya, saling dukung, jangan suka ngilang ya sayang, inget ya kamu ga sendirian ada aku",
-        "aku terima kasih banget kamu udah mau sabar udah mau bertahan sampe sejauh ini, aku bangga sama kamu",
-        "kamu kuat kamu keren walaupun kayak tai kadang kelauannya, cuma yaudah gapapa ya",
-        "adaa aku ya sayangku dindaku, jangan pernah ngerasa sendiri, walaupun kita jauh tapi kita menatap layar terus",
-        "soalnya kan kia gapernah keluar rumah jadi ga menatap langit",
-        "bercanda ya sayang, intinya semoga di umur hubungan kita yang setahun ini diberi kesehatan, kelancaran, langgeng, rezeki yang lancar, dan",
-        "semoga plan yang udah kita buat semua terwujud ya sayangkuu dindakuu, Aamiin....", 
-        "pinjam dulu seratus nanti ku ganti", 
-        "aku dalam segenap jiwa bener-bener sayang banget sama kamu dindaku", 
-        "Aku Sayang Banget Sama Kamu", 
-        "I Love You Ayu Dinda Nurani"],
+        strings: ["Hay sayangku dindakuu istriku mbuku",
+            "I just wanna say",
+            "Happy Anniversary",
+            "Everyday kalo sama kamu bawaannya happy banget",
+            "I just wanna say thank you for everything",
+            "Terima kasih udah selalu ada, mau sabar dengan sikapku yang random wkwkwk….makasih ya mbu",
+            "Kita udah setahun ya…",
+            "Ga kerasa yaa sayang, walaupun kita ngerayainnya LDR",
+            "Tapi aku tetep bersyukur karna kita masih bisa bareng sama kamu gapapa LDR ngeluh dikit lah ya wkwkwk",
+            "Kita sejauh 446KM ya 5-6 jam untuk aku bisa ketemu kamu",
+            "Sedih yaa tapi gapapa, kita bisa melewati ini semua",
+            "Aku cuma mau kamu inget 5 hal",
+            "1. Jangan aneh aneh",
+            "2. Jangan sering ngilang",
+            "3. Kabarin terus kemanapun dimanapun berkabar",
+            "4. Inget aku yaa sayaang xixixixixi",
+            "5. Jangan pernah ngerasa kamu sendirian, kamu punya aku, libatkan aku dalam segala apapun, ceritaa apapun ituu sama aku jangan babibubebo okkeeyyy",
+            "mungkin itu 5 hal dari aku", 
+            "semogaa di hubungan kita yang setahun ini kita langgeng terus sehat terus rezeki lancarr terus, semoga jugaa plan yang udah kita buat terjadi semua yaa sayaang Aamiin… ",
+            
+            "I love you Ayu Dinda Nurani",
+            "Kamu Tidak Sendirian, kamu ada aku #KitaSelaluMenguatkan"],
         typeSpeed: 80
     });
 }
+
