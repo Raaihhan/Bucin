ccbd51f1769e528030245a4b888294cbed04c568
Raaihhan <lenerjejerin@gmail.com>
2024-11-23 19:10:08 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Hayy Sayangku Dindakuu", "Cuma mau bilang", "Happy Anniversary", "Anjayy udah setahun aja brok ", "Betah-betah yaa jiwanya awoakwokaowk ", "Eh tapi ini serius dulu kali ya", "maaf ya kita harus LDR gini, anniv malah LDR", "inget ya sayang kita harus saling", "pinjam dulu seratus nanti ku ganti", "aku dalam segenap jiwa bener-bener sayang banget sama kamu dindaku", "Aku Sayang Banget Sama Kamu", "I Love You Ayu Dinda Nurani"],
+        typeSpeed: 70
+    });
+}
